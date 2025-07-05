6e4426a2662dffc80d47d6080adc23ecd04b0cec
KosmicDaKerbal <ankurparchure@gmail.com>
2025-07-05 20:28:10 +0530

    3

--- src/commands/captcha.js
+++ src/commands/captcha.js
@@ -1,11 +1,14 @@
 const { EmbedBuilder } = require("discord.js");
 module.exports = {
-    start: async function (embed, captchaObject){
+    start: async function (embed, captchaObject, client){
           const index = new EmbedBuilder().setTitle("Captcha Verification Process Started. Check your DM's.");
           await embed.reply({ embeds: [index], ephemeral: true });
           captchaObject.present(embed.member);
           captchaObject.on("success", data => {
           console.log(`${data.member.user.username} has solved a CAPTCHA.`);
+          const channel = client.channels.cache.get('1389110949436330014');
+          index.setTitle(`${data.member.user.username} has solved a CAPTCHA.`);
+          channel.send({ embeds: [index]});
           try {
             data.member.roles.remove("1368095911305281536");
           } catch (e){
