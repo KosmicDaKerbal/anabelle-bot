c3ba5393dcb53c957dbb1ab5b23228a370524d63
KosmicDaKerbal <ankurparchure@gmail.com>
2025-07-05 20:30:35 +0530

    4

--- src/commands/captcha.js
+++ src/commands/captcha.js
@@ -7,7 +7,7 @@ module.exports = {
           captchaObject.on("success", data => {
           console.log(`${data.member.user.username} has solved a CAPTCHA.`);
           const channel = client.channels.cache.get('1389110949436330014');
-          index.setTitle(`${data.member.user.username} has solved a CAPTCHA.`);
+          index.setTitle(`<@${data.member.user.id}> has solved a CAPTCHA.`);
           channel.send({ embeds: [index]});
           try {
             data.member.roles.remove("1368095911305281536");
