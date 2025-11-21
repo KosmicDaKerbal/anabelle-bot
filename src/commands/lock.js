51ebf6654de1ddf01a3d6082c20ba2cb3351b813
KosmicDaKerbal <ankurparchure@gmail.com>
2025-11-22 00:26:31 +0530

    5

--- src/commands/lock.js
+++ src/commands/lock.js
@@ -11,7 +11,7 @@ module.exports = {
     const res = new EmbedBuilder().setTitle("Channel Locked").setColor(0x8c3f7a).setTimestamp();
 //    const duration = embed.options.get("duration").value;
     try {
-        lockchannel.overwritePermissions([
+        await lockchannel.permissionOverwrites.set([
             {
             id: process.env.VERIFIED_ROLE,
             allow: [
