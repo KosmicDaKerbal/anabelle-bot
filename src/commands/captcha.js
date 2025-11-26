7793bddf32e86c0650c01abd0c7fd6d73d2d9421
KosmicDaKerbal <ankurparchure@gmail.com>
2025-11-26 12:54:41 +0530

    3

--- src/commands/captcha.js
+++ src/commands/captcha.js
@@ -4,10 +4,9 @@ module.exports = {
     start: async function (embed, captchaObject, channel){
           const index = new EmbedBuilder().setTitle("Captcha Verification Process Started. Check your DM's.");
           await embed.reply({ embeds: [index], flags: MessageFlags.Ephemeral });
-          captchaObject.present(embed.member);
+          //captchaObject.present(embed.member);
           captchaObject.on("success", async data => {
           console.log(`${data.member.user.username} has solved a CAPTCHA.`);
-          //const channel = client.channels.cache.get(process.env.GHCHAT_ID);
           index.setTitle(`${data.member.user.username} i̶͝ͅs̴̹̚ ̸̘́h̶͚͗e̵̛̼r̸͈͛ë̷̫́ ̴͎̿t̷̙̓o̸̜̐ ̷̺̀p̵̜͗l̴̮̓a̸̬͗y̸̬̆`);
           await channel.send({ embeds: [index]});
           try {
