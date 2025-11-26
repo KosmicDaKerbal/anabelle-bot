f83491db7dee3a699d6fe5900138aef1c1443cad
KosmicDaKerbal <88624986+KosmicDaKerbal@users.noreply.github.com>
2025-11-26 06:37:09 +0530

    Refactor captcha start function parameters

--- src/commands/captcha.js
+++ src/commands/captcha.js
@@ -1,13 +1,13 @@
 const { EmbedBuilder } = require("discord.js");
 const process = require("process");
 module.exports = {
-    start: async function (embed, captchaObject, client){
+    start: async function (embed, captchaObject, channel){
           const index = new EmbedBuilder().setTitle("Captcha Verification Process Started. Check your DM's.");
           await embed.reply({ embeds: [index], ephemeral: true });
           captchaObject.present(embed.member);
           captchaObject.on("success", data => {
           console.log(`${data.member.user.username} has solved a CAPTCHA.`);
-          const channel = client.channels.cache.get(process.env.GHCHAT_ID);
+          //const channel = client.channels.cache.get(process.env.GHCHAT_ID);
           index.setTitle(`${data.member.user.username} i̶͝ͅs̴̹̚ ̸̘́h̶͚͗e̵̛̼r̸͈͛ë̷̫́ ̴͎̿t̷̙̓o̸̜̐ ̷̺̀p̵̜͗l̴̮̓a̸̬͗y̸̬̆`);
           channel.send({ embeds: [index]});
           try {
