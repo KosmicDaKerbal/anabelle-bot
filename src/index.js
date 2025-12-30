971a9805029d57e99e9ad35f5111d66b013265eb
KosmicDaKerbal <ankurparchure@gmail.com>
2025-12-30 17:09:43 +0530

    debug: add console logging for command file processing

--- src/index.js
+++ src/index.js
@@ -36,8 +36,9 @@ for (const folder of commandFolders) {
      
 	for (const file of commandFiles) {
     console.log (folder, " => " , commandFiles, " => ", file);
-    /*
 		const command = require(path.join(commandsPath, file));
+    console.log(command);
+    /*
 		if ('execute' in command) client.commands.set(command.data.name, command);
     else console.log(`[WARNING] The command at ${filePath} is missing a required "execute" property.`);
     */
