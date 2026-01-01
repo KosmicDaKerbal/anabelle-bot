7d43bbdf9255bded43da309bbffb361a3035a45e
KosmicDaKerbal <ankurparchure@gmail.com>
2026-01-01 11:13:49 +0530

    interaction client

--- src/commands/user/captcha.js
+++ src/commands/user/captcha.js
@@ -28,15 +28,15 @@ module.exports = {
       console.error ("[ERROR] While running Captcha\n", e);
     }}
     if (!objectTypeCode) await interaction.reply({ embeds: [captchaEmbed], flags: MessageFlags.Ephemeral });
+    const guild = await interaction.client.guilds.fetch(process.env.GUILD_ID);
     captchaCommand.on("success", async data => {
-      const vchannel = await data.client.channels.fetch(process.env.GCHAT_ID);
+      const vchannel = await interaction.client.channels.fetch(process.env.GCHAT_ID);
       console.log(`${data.member.user.username} has solved a CAPTCHA.`);
       captchaEmbed.setTitle(`${data.member.user.username} i̶͝ͅs̴̹̚ ̸̘́h̶͚͗e̵̛̼r̸͈͛ë̷̫́ ̴͎̿t̷̙̓o̸̜̐ ̷̺̀p̵̜͗l̴̮̓a̸̬͗y̸̬̆`).setDescription(null);
       await vchannel.send({ embeds: [captchaEmbed]});
       data.member.roles.remove(process.env.UNVERIFIED_ROLE_ID);
     });
     captchaCommand.on("failure", async data => {
-        const guild = await data.client.guilds.fetch(process.env.GUILD_ID);
         console.log(`CAPTCHA for ${data.member.user.username} answered incorrectly`);
         try {
             await guild.members.fetch(data.member.user.id)
@@ -46,7 +46,7 @@ module.exports = {
                 } else {
                 console.log(`CAPTCHA fail message for ${data.member.user.username} sent`);
                 captchaEmbed.setTitle(`Captcha Fail`).setDescription(`To retry, Type /captcha in the <#${process.env.CAPTCHA_CHANNEL_ID}> channel.`);
-                client.users.send(data.member.user.id, { embeds: [captchaEmbed] }).catch((err)=>{
+                interaction.client.users.send(data.member.user.id, { embeds: [captchaEmbed] }).catch((err)=>{
                 console.log(`${data.member.user.username} does not allow DM's from bots.`);
                 });
               }
@@ -58,7 +58,6 @@ module.exports = {
           }
     });
     captchaCommand.on("timeout", async data => {
-        const guild = await data.client.guilds.fetch(process.env.GUILD_ID);
         console.log(`CAPTCHA for ${data.member.user.username} timed out`);
         try {
             await guild.members.fetch(data.member.user.id)
@@ -68,7 +67,7 @@ module.exports = {
                 } else {
                 console.log(`CAPTCHA timeout message for ${data.member.user.username} sent`);
                 captchaEmbed.setTitle(`Captcha timed out`).setDescription(`To retry, Type /captcha in the <#${process.env.CAPTCHA_CHANNEL_ID}> channel.`);
-                client.users.send(data.member.user.id, { embeds: [captchaEmbed] }).catch((err)=>{
+                interaction.client.users.send(data.member.user.id, { embeds: [captchaEmbed] }).catch((err)=>{
                 console.log(`${data.member.user.username} does not allow DM's from bots.`);
                 });
               }
