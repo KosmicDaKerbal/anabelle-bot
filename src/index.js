10c442e6ebec7c0774f4654a5a3762706574d76b
KosmicDaKerbal <ankurparchure@gmail.com>
2026-01-01 13:12:40 +0530

    1

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ client.on(Events.InteractionCreate, async (mainInteraction) => {
       {
         type: (process.env.MAINTANENCE_MODE === "0") ? ActivityType.Watching : ActivityType.Custom,
         name: "you",
-        state: "--MAINTANENCE MODE--"
+        //state: "--MAINTANENCE MODE--"
       }
     ],
       status: 'idle'
