1bcb0756f58ae2a7b19973e2d2a9b1d0429df943
KosmicDaKerbal <ankurparchure@gmail.com>
2026-01-01 12:38:06 +0530

    2

--- src/commands/user/help.js
+++ src/commands/user/help.js
@@ -8,9 +8,8 @@ module.exports = {
   data: new SlashCommandBuilder().setName('help').setDescription("Complete commands list for the bot"),
   async execute (interaction) {
     const commandsList = [];
-    console.log (interaction.client.commands);
-    for (const command of interaction.client.commands){ commandsList.push ({"name": `"/${command.data.name}"`, "value": `"${command.data.description}"`});
-  console.log (command)}
+    for (const command of interaction.client.commands){ //commandsList.push ({"name": `"/${command.data.name}"`, "value": `"${command.data.description}"`});
+  console.log (command.data)}
     const help = new EmbedBuilder().setTitle("Help Section").setColor(0x8c3f7a).addFields(JSON.stringify(commandsList)).setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL({ dynamic: true, size: 32 })}).setTimestamp();
     await interaction.reply({ embeds: [help] });
   }
