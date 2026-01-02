471d26cd7931dbbe85c065220b463c8c7cb6cb68
KosmicDaKerbal <ankurparchure@gmail.com>
2026-01-02 21:08:47 +0530

    1

--- src/commands/admin/config.js
+++ src/commands/admin/config.js
@@ -1,12 +1,5 @@
 const {EmbedBuilder, SlashCommandBuilder} = require("discord.js");
 const process = require("process");
-const dbOptions = {
-  readonly: false,
-  fileMustExist: false,
-  timeout: 5000,
-  verbose: console.log
-};
-const db = require('better-sqlite3')(`${__dirname}/../../serverConfig.db`, dbOptions);
 module.exports = {
   data: new SlashCommandBuilder().setName('config').setDescription("**Admin Command**: Open Configuration Menu")
   .addSubcommand((subcommand) => subcommand.setName('setup').setDescription('**Admin Command**: Defines all important roles and channels for the bot to work.'))
