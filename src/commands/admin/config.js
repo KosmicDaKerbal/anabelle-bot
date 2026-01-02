e090b228e4a527c4f7a63d1d53fd2672a04d86e1
KosmicDaKerbal <ankurparchure@gmail.com>
2026-01-03 00:08:04 +0530

    change interaction

--- src/commands/admin/config.js
+++ src/commands/admin/config.js
@@ -30,7 +30,7 @@ module.exports = {
             const unverifiedRole = new LabelBuilder().setLabel("Select the server's unverified role").setDescription('This role will be given to new members have not yet solved a CAPTCHA').setRoleSelectMenuComponent(unverifiedRoleSelect);
             const botsRole = new LabelBuilder().setLabel("Select the server's bots role").setDescription('This role will be given to newly added bots. No CAPTCHA will be asked to them.').setRoleSelectMenuComponent(botsRoleSelect);
             configRolesModal.addLabelComponents(verifiedRole, unverifiedRole, botsRole);
-            await interaction.showModal(configRolesModal);
+            await Form.showModal(configRolesModal);
             break;
         case 'mod-teamButton':
             const configModsModal = new ModalBuilder().setCustomId('configMods').setTitle('Server Mod Team Configuration for Anabelle');
@@ -43,7 +43,7 @@ module.exports = {
             const adminRole = new LabelBuilder().setLabel("Select the server's administrator role(s)").setDescription('Select up to 2 roles for administrators of the server').setRoleSelectMenuComponent(adminRoleSelect);
             const owner = new LabelBuilder().setLabel("Select the server's owner").setDescription("Select the server's owner").setUserSelectMenuComponent(ownerSelect);
             configModsModal.addLabelComponents(juniorModRole, seniorModRole, adminRole, owner);
-            await interaction.showModal(configModsModal);
+            await Form.showModal(configModsModal);
             break;
         case 'channelsButton':
             const configChannelsModal = new ModalBuilder().setCustomId('configChannels').setTitle('Server Channels Configuration for Anabelle');
@@ -54,7 +54,7 @@ module.exports = {
             const logChannel = new LabelBuilder().setLabel("Select the bot's logging channel").setDescription('Select a channel where the bot can send actions log.').setChannelSelectMenuComponent(logChannelSelect);
             const welcomeChannel = new LabelBuilder().setLabel("Select the bot's welcome channel").setDescription('Select a channel where the bot can send user welcome messages.').setChannelSelectMenuComponent(welcomeChannelSelect);
             configChannelsModal.addLabelComponents(verificationChannel, logChannel, welcomeChannel);
-            await interaction.showModal(configChannelsModal);
+            await Form.showModal(configChannelsModal);
             break;
         }
     });
