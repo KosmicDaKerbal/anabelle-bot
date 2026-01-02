ef3afba177dff60b64123c3b1b2cf2cf6dcf6404
KosmicDaKerbal <ankurparchure@gmail.com>
2026-01-02 23:27:03 +0530

    delimit form

--- src/commands/admin/config.js
+++ src/commands/admin/config.js
@@ -3,40 +3,53 @@ const process = require("process");
 const { log } = require("util");
 module.exports = {
   data: new SlashCommandBuilder().setName('config').setDescription("**Admin Command**: Open Configuration Menu")
-  .addSubcommand((subcommand) => subcommand.setName('setup').setDescription('**Admin Command**: Defines all important roles and channels for the bot to work.'))
+  .addSubcommand((subcommand) => subcommand.setName('roles').setDescription('**Admin Command**: Defines all important roles for the bot to work properly.'))
+  .addSubcommand((subcommand) => subcommand.setName('mod-team').setDescription('**Admin Command**: Defines all important admin roles and users for the bot to work properly.'))
+  .addSubcommand((subcommand) => subcommand.setName('channels').setDescription('**Admin Command**: Defines all important channels for the bot to work properly.'))
   .addSubcommand((subcommand) => subcommand.setName('show').setDescription('**Admin Command**: Shows the configuration of the bot'))
   .addSubcommand((subcommand) => subcommand.setName('edit').setDescription('**Admin Command**: Edit the bot configuration'))
   .addSubcommand((subcommand) => subcommand.setName('delete').setDescription('**Admin Command**: Remove all configurations')),
   async execute (interaction) {
-    const configEmbed = new EmbedBuilder();
+    const configEmbed = new EmbedBuilder().setTitle('Opening bot configuration interface...');
+    interaction.client.db.exec(`INSERT INTO config(guildID) VALUES(${interaction.guild.id}) ON CONFLICT DO UPDATE SET guildID = ${interaction.guild.id}`);
     switch (interaction.options.getSubcommand()){
-        case 'setup':
-            configEmbed.setTitle('Opening bot configuration interface...');
-            interaction.client.db.exec(`INSERT INTO config(guildID) VALUES(${interaction.guild.id}) ON CONFLICT DO UPDATE SET guildID = ${interaction.guild.id}`);
-            const configModal = new ModalBuilder().setCustomId('config').setTitle('Server Configuration for Anabelle');
+        case 'roles':       
+            const configRolesModal = new ModalBuilder().setCustomId('configRoles').setTitle('Server Roles Configuration for Anabelle');
             const verifiedRoleSelect = new RoleSelectMenuBuilder().setCustomId('vRole').setPlaceholder('Select a role').setMaxValues(1);
             const unverifiedRoleSelect = new RoleSelectMenuBuilder().setCustomId('uvRole').setPlaceholder('Select a role').setMaxValues(1);
             const botsRoleSelect = new RoleSelectMenuBuilder().setCustomId('bRole').setPlaceholder('Select a role').setMaxValues(1);
-            const juniorModRoleSelect = new RoleSelectMenuBuilder().setCustomId('jmRole').setPlaceholder('Select a role').setMaxValues(2);
-            const seniorModRoleSelect = new RoleSelectMenuBuilder().setCustomId('smRole').setPlaceholder('Select a role').setMaxValues(2);
-            const adminRoleSelect = new RoleSelectMenuBuilder().setCustomId('adRole').setPlaceholder('Select a role').setMaxValues(2);
-            const logChannelSelect = new ChannelSelectMenuBuilder().setCustomId('lChannel').setPlaceholder('Select a channel').setMaxValues(1);
-            const verificationChannelSelect = new ChannelSelectMenuBuilder().setCustomId('vChannel').setPlaceholder('Select a channel').setMaxValues(1);
-            const welcomeChannelSelect = new ChannelSelectMenuBuilder().setCustomId('wChannel').setPlaceholder('Select a channel').setMaxValues(1);
-            const ownerSelect = new UserSelectMenuBuilder().setCustomId('owner').setPlaceholder('Select user').setMaxValues(1);
             const verifiedRole = new LabelBuilder().setLabel("Select the server's verified role").setDescription('This role will be given to new members who successfully solve a CAPTCHA.').setRoleSelectMenuComponent(verifiedRoleSelect);
             const unverifiedRole = new LabelBuilder().setLabel("Select the server's unverified role").setDescription('This role will be given to new members have not yet solved a CAPTCHA').setRoleSelectMenuComponent(unverifiedRoleSelect);
             const botsRole = new LabelBuilder().setLabel("Select the server's bots role").setDescription('This role will be given to newly added bots. No CAPTCHA will be asked to them.').setRoleSelectMenuComponent(botsRoleSelect);
-            const verificationChannel= new LabelBuilder().setLabel("Select the bot's verification channel").setDescription('Select a channel where the verification messages can be sent in case the user has disabled DMs.').setChannelSelectMenuComponent(verificationChannelSelect);
-            const logChannel = new LabelBuilder().setLabel("Select the bot's logging channel").setDescription('Select a channel where the bot can send actions log.').setChannelSelectMenuComponent(logChannelSelect);
-            const welcomeChannel = new LabelBuilder().setLabel("Select the bot's welcome channel").setDescription('Select a channel where the bot can send user welcome messages.').setChannelSelectMenuComponent(welcomeChannelSelect);
+            configRolesModal.addLabelComponents(verifiedRole, unverifiedRole, botsRole);
+            await interaction.reply({embeds: [configEmbed], flags: MessageFlags.Ephemeral});
+            await interaction.showModal(configRolesModal);
+            break;
+        case 'mod-team':
+            const configModsModal = new ModalBuilder().setCustomId('configMods').setTitle('Server Mod Team Configuration for Anabelle');
+            const juniorModRoleSelect = new RoleSelectMenuBuilder().setCustomId('jmRole').setPlaceholder('Select a role').setMaxValues(2);
+            const seniorModRoleSelect = new RoleSelectMenuBuilder().setCustomId('smRole').setPlaceholder('Select a role').setMaxValues(2);
+            const adminRoleSelect = new RoleSelectMenuBuilder().setCustomId('adRole').setPlaceholder('Select a role').setMaxValues(2);
+            const ownerSelect = new UserSelectMenuBuilder().setCustomId('owner').setPlaceholder('Select user').setMaxValues(1);
             const juniorModRole = new LabelBuilder().setLabel("Select the server's junior mod role(s)").setDescription('Select up to 2 roles for temporary admins, trial mods etc.').setRoleSelectMenuComponent(juniorModRoleSelect);
             const seniorModRole = new LabelBuilder().setLabel("Select the server's senior mod role(s)").setDescription('Select up to 2 roles for permanent moderators').setRoleSelectMenuComponent(seniorModRoleSelect);
             const adminRole = new LabelBuilder().setLabel("Select the server's administrator role(s)").setDescription('Select up to 2 roles for administrators of the server').setRoleSelectMenuComponent(adminRoleSelect);
             const owner = new LabelBuilder().setLabel("Select the server's owner").setDescription("Select the server's owner").setUserSelectMenuComponent(ownerSelect);
-            configModal.addLabelComponents(verifiedRole, unverifiedRole, botsRole, verificationChannel, logChannel, welcomeChannel, juniorModRole, seniorModRole, adminRole, owner);
-            await interaction.showModal(configModal);
-            interaction.reply({embeds: [configEmbed], flags: MessageFlags.Ephemeral});
+            configModsModal.addLabelComponents(juniorModRole, seniorModRole, adminRole, owner);
+            await interaction.reply({embeds: [configEmbed], flags: MessageFlags.Ephemeral});
+            await interaction.showModal(configModsModal);
+            break;
+        case 'channels':
+            const configChannelsModal = new ModalBuilder().setCustomId('configChannels').setTitle('Server Channels Configuration for Anabelle');
+            const logChannelSelect = new ChannelSelectMenuBuilder().setCustomId('lChannel').setPlaceholder('Select a channel').setMaxValues(1);
+            const verificationChannelSelect = new ChannelSelectMenuBuilder().setCustomId('vChannel').setPlaceholder('Select a channel').setMaxValues(1);
+            const welcomeChannelSelect = new ChannelSelectMenuBuilder().setCustomId('wChannel').setPlaceholder('Select a channel').setMaxValues(1);
+            const verificationChannel= new LabelBuilder().setLabel("Select the bot's verification channel").setDescription('Select a channel where the verification messages can be sent in case the user has disabled DMs.').setChannelSelectMenuComponent(verificationChannelSelect);
+            const logChannel = new LabelBuilder().setLabel("Select the bot's logging channel").setDescription('Select a channel where the bot can send actions log.').setChannelSelectMenuComponent(logChannelSelect);
+            const welcomeChannel = new LabelBuilder().setLabel("Select the bot's welcome channel").setDescription('Select a channel where the bot can send user welcome messages.').setChannelSelectMenuComponent(welcomeChannelSelect);
+            configChannelsModal.addLabelComponents(verificationChannel, logChannel, welcomeChannel);
+            await interaction.reply({embeds: [configEmbed], flags: MessageFlags.Ephemeral});
+            await interaction.showModal(configChannelsModal);
             break;
     }
   }
