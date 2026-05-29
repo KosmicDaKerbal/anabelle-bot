531dd221f23947e93478370fe9c5aa95082647fe
KosmicDaKerbal <ankurparchure@gmail.com>
2026-05-29 12:36:56 +0530

    remove envionment variables

--- src/commands/user/captcha.js
+++ src/commands/user/captcha.js
@@ -5,7 +5,7 @@ module.exports = {
   async execute (interaction, objectTypeCode) {
     const guild = (!objectTypeCode) ? interaction.member.guild : interaction.guild;
     const captchaEmbed = new EmbedBuilder().setTitle("Captcha verification process started. Check your DM's.").setDescription(`If your DM's are closed, check the verification channel. If that doesn't work, please open your DM's temporarily.`).setColor(0xffff00);
-    const localData = interaction.client.db.prepare("SELECT unverifiedRoleID, verifiedRoleID, verificationChannelID, welcomeChannelID FROM localConfig WHERE guildID = ?").get(guild.id);
+    const localData = interaction.client.db.prepare("SELECT unverifiedRoleID, verifiedRoleID, verificationChannelID, welcomeChannelID, logChannelID FROM localConfig WHERE guildID = ?").get(guild.id);
     const captchaCommand = new Captcha(interaction.client, {
     roleID: localData.verifiedRoleID,
     channelID: localData.verificationChannelID,
@@ -20,7 +20,7 @@ module.exports = {
     customSuccessEmbed: new EmbedBuilder().setTitle("I̶̡͠ ̶͓͝l̷̬̒i̷̳͘ķ̴̃e̶͍͝ ̶̦͐ỷ̶̦o̴̰͝ú̸̝.̵͇͘").setImage(process.env.CAPTCHA_SUCCESS).setFooter({ text: 'Captcha verification complete', iconURL: guild.iconURL({ dynamic: true, size: 32 })}).setTimestamp().setColor(0x00ff00),
     customFailureEmbed: new EmbedBuilder().setTitle("Ī̵̮ ̴̥̒c̵̝͋a̶̺͘n̴̤͑'̶͚̋t̶̳̿ ̶̥͌p̵̦̒l̴͈̓a̵̹͝ȳ̷̭ ̶͓̈́ẃ̷̘ĭ̶͎t̸̹͐h̶̆͜ ̵͈̎ỳ̶̯o̸̹͗u̶̙͆").setImage(process.env.CAPTCHA_FAIL).setFooter({ text: 'Captcha verification failed', iconURL: guild.iconURL({ dynamic: true, size: 32 })}).setTimestamp().setColor(0xff0000),
     });
-    if (!objectTypeCode && interaction.member.roles.cache.some(role => role.name === 'Verified')) captchaEmbed.setTitle("User already verified").setDescription("You have texting permissions on the server.").setColor(0x00ff00);
+    if (!objectTypeCode && interaction.member.roles.cache.has(localData.verifiedRoleID)) captchaEmbed.setTitle("User already verified").setDescription("You have texting permissions on the server.").setColor(0x00ff00);
     else {
       try {
       captchaCommand.present((!objectTypeCode) ? interaction.member : interaction);
@@ -31,11 +31,11 @@ module.exports = {
     }}
     if (!objectTypeCode) await interaction.reply({ embeds: [captchaEmbed], flags: MessageFlags.Ephemeral });
     captchaCommand.on("success", async data => {
-      const vchannel = await interaction.client.channels.fetch(process.env.GCHAT_ID);
+      const vchannel = await interaction.client.channels.fetch(localData.welcomeChannelID);
       console.log(`[INFO] ${data.member.user.username} has solved a CAPTCHA, ID: ${data.member.user.id}`);
       captchaEmbed.setTitle(`ẁ̸̡̛̱e̸̬̙̚l̴̏̇ͅĉ̴͎͘ọ̷̄̒m̵̻̬̅͝ȅ̶̹͝`).setDescription(`<@${data.member.user.id}> i̶͝ͅs̴̹̚ ̸̘́h̶͚͗e̵̛̼r̸͈͛ë̷̫́ ̴͎̿t̷̙̓o̸̜̐ ̷̺̀p̵̜͗l̴̮̓a̸̬͗y̸̬̆`).setColor(0x8c3f7a);
       await vchannel.send({ embeds: [captchaEmbed]});
-      data.member.roles.remove(process.env.UNVERIFIED_ROLE_ID);
+      data.member.roles.remove(localData.unverifiedRoleID);
     });
     captchaCommand.on("failure", async data => {
         console.log(`[INFO] CAPTCHA for ${data.member.user.username} answered incorrectly, ID: ${data.member.user.id}`);
@@ -44,7 +44,7 @@ module.exports = {
                 console.log(`[INFO] ${data.member.user.username} has left the server, ID: ${data.member.user.id}`);
                 } else {
                 console.log(`[INFO] CAPTCHA fail message for ${data.member.user.username} sent, ID: ${data.member.user.id}`);
-                captchaEmbed.setTitle("Captcha Fail").setDescription(`To retry, Type /captcha in the <#${process.env.CAPTCHA_CHANNEL_ID}> channel.`).setFooter({ text: data.member.guild.name, iconURL: data.member.guild.iconURL({ dynamic: true, size: 32 })}).setTimestamp().setColor(0xff0000);
+                captchaEmbed.setTitle("Captcha Fail").setDescription(`To retry, Type /captcha in the <#${localData.verificationChannelID}> channel.`).setFooter({ text: data.member.guild.name, iconURL: data.member.guild.iconURL({ dynamic: true, size: 32 })}).setTimestamp().setColor(0xff0000);
                 interaction.client.users.send(data.member.user.id, { embeds: [captchaEmbed] }).catch((e)=>{
                 console.log(`[INFO] ${data.member.user.username} does not allow DM's from bots, ID: ${data.member.user.id}`);
                 });
@@ -60,7 +60,7 @@ module.exports = {
                 console.log(`[INFO] ${data.member.user.username} has left the server, ID: ${data.member.user.id}`);
                 } else {
                 console.log(`[INFO] CAPTCHA timeout message for ${data.member.user.username} sent, ID: ${data.member.user.id}`);
-                captchaEmbed.setTitle("Captcha Timeout").setDescription(`To retry, Type /captcha in the <#${process.env.CAPTCHA_CHANNEL_ID}> channel.`).setFooter({ text: data.member.guild.name, iconURL: data.member.guild.iconURL({ dynamic: true, size: 32 })}).setTimestamp().setColor(0xff0000);
+                captchaEmbed.setTitle("Captcha Timeout").setDescription(`To retry, Type /captcha in the <#${localData.verificationChannelID}> channel.`).setFooter({ text: data.member.guild.name, iconURL: data.member.guild.iconURL({ dynamic: true, size: 32 })}).setTimestamp().setColor(0xff0000);
                 interaction.client.users.send(data.member.user.id, { embeds: [captchaEmbed] }).catch((e)=>{
                 console.log(`[INFO] ${data.member.user.username} does not allow DM's from bots, ID: ${data.member.user.id}`);
                 });
